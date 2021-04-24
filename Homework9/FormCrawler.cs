114acbc29ec2e55610b28c874c2ed19e79d63940
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-04-24 23:41:07 +0800

    Upload Homework9

---
+++ Homework9/FormCrawler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework9
+{
+    public partial class FormCrawler : Form
+    {
+        public string StartUrl { get; set; }
+
+        private SimpleCrawler simpleCrawler = new SimpleCrawler();
+
+        public FormCrawler()
+        {
+            InitializeComponent();
+            simpleCrawler.statusChange += RefreshStatus;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textWebsite.DataBindings.Add("Text", this, "StartUrl");
+            radioCurrentYes.DataBindings.Add("Checked", simpleCrawler, "OnlyCurrent");
+            bindingSourceUrl.DataSource = simpleCrawler.Urls;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (StartUrl == null || StartUrl == "")
+            {
+                if (MessageBox.Show("请输入爬取的网站") == DialogResult.OK) return;
+            }
+            if (!IsValid(StartUrl))
+            {
+                if (MessageBox.Show("网站输入不合法") == DialogResult.OK) return;
+            }
+            simpleCrawler.PrepareCrawl(textWebsite.Text);
+        }
+
+        private bool IsValid(string website)
+        {
+            try
+            {
+                Uri uri = new Uri(website);
+                return true;
+            }
+            catch (UriFormatException)
+            {
+                return false;
+            }
+        }
+
+        // 刷新状态信息
+        private void RefreshStatus(string str)
+        {
+            labStatus.Text = str;
+            labStatus.Refresh();
+            bindingSourceUrl.ResetBindings(false);
+        }
+    }
+}
---
+++ Homework9/Url.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{
+    public class Url
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+
+        public Url(string name, string message)
+        {
+            this.Name = name;
+            this.Message = message;
+        }
+    }
+}
